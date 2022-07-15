1ac3c06de040da101ee4180d59d17cc85d71de04
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2022-07-15 14:26:00 +0400

    Displaying API information in a component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,39 @@ import Weather from "./components/Weather";
 const API_KEY = "647da832546c5d35f3a576246c75e8b8";
 
 class App extends React.Component {
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    pressure: undefined,
+    sunset: undefined,
+    error: undefined,
+  };
+
   gettingWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
-    const api_url = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    ).then((data) => data.json());
-    console.log(api_url);
+
+    if (city) {
+      const data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      ).then((api_url) => api_url.json());
+
+      let sunset = data.sys.sunset;
+      let date = new Date();
+      date.setTime(sunset);
+      let sunset_date =
+        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        pressure: data.main.pressure,
+        sunset: sunset_date,
+        error: "",
+      });
+    }
   };
 
   render() {
@@ -20,7 +46,14 @@ class App extends React.Component {
       <>
         <Info />
         <Form weatherMethod={this.gettingWeather} />
-        <Weather />
+        <Weather
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          pressure={this.state.pressure}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
       </>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,20 @@ import React from "react";
 
 class Weather extends React.Component {
   render() {
-    return <div>Weather</div>;
+    return (
+      <div>
+        {this.props.city && (
+          <>
+            <p>
+              Location: {this.props.city}, {this.props.country}
+            </p>
+            <p>Temperature: {this.props.temp}</p>
+            <p>Pressure: {this.props.pressure}</p>
+            <p>Sunset: {this.props.sunset}</p>
+          </>
+        )}
+      </div>
+    );
   }
 }
 
